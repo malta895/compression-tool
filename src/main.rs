f928854bd0a737be91b2be37ea7d7faf460ea1e9
Mattia Fumo <mattia.fumo@mia-platform.eu>
2024-07-10 12:26:45 +0200

    huffman module

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,127 @@
 use std::{collections::HashMap, io::Read};
 
+mod huffman {
+    use std::{cmp::Ordering, collections::{BinaryHeap, HashMap}};
+
+    #[derive(PartialEq, PartialOrd, Eq)]
+    enum Tree {
+        Leaf{
+            data: char,
+            freq: u64,
+        },
+        Node{
+            freq: u64,
+            left: Box<Tree>,
+            right: Box<Tree>,
+        },
+    }
+
+    impl Tree {
+        fn freq(&self) -> u64 {
+            match self {
+                Tree::Leaf { freq, .. } => *freq,
+                Tree::Node { freq, .. } => *freq
+            }
+        }
+    }
+
+    impl Ord for Tree {
+        fn cmp(&self, other: &Self) -> Ordering {
+            let freq1 = match self {
+                Tree::Leaf {freq, ..} => freq,
+                Tree::Node { freq, .. } => freq
+            };
+
+            let freq2 = match other {
+                Tree::Leaf {freq, ..} => freq,
+                Tree::Node { freq, .. } => freq
+            };
+
+            freq1.cmp(freq2)
+        }
+    }
+
+    #[derive(Clone, Debug)]
+    pub struct Symbol {
+        data: Vec<bool>
+    }
+
+    impl Symbol {
+        fn new() -> Symbol {
+            Symbol { data: vec![] }
+        }
+
+        fn append(&self, unit: bool) -> Symbol {
+            Symbol { data: vec![ self.data.clone(), vec![unit]].concat() }
+        }
+    }
+
+    pub fn encode(freq_table: &HashMap<char, u64>) -> Vec<(char, Symbol)> {
+        let forest = freq_table.iter().map(|(ch, freq)| Tree::Leaf { data: *ch, freq: *freq }).collect::<Vec<Tree>>();
+        let mut heap: BinaryHeap<Tree> = BinaryHeap::from(forest);
+        
+        let mut tree: Option<Tree> = None;
+
+        loop {
+            let t1 = match heap.pop() {
+                None => break,
+                Some(t1) => t1
+            };
+            
+            let t2 = match heap.pop() {
+                None => {
+                    tree = Some(t1);
+                    break;
+                },
+                Some(t2) => t2
+            };
+
+            let t = Tree::Node { 
+                freq: t1.freq() + t2.freq(), 
+                left: Box::new(t1), 
+                right: Box::new(t2)
+            };
+
+            heap.push(t);
+        }
+
+        fn build_symbol_table(tree: Tree, path: Option<Symbol>) -> Vec<(char, Symbol)> {
+            let path = path.unwrap_or(Symbol::new());
+            match tree {
+                Tree::Leaf { data, .. } => vec![(data, path)],
+                Tree::Node { left, right, .. } => vec![
+                    build_symbol_table(*left, Some(path.append(true))),
+                    build_symbol_table(*right, Some(path.append(false)))
+                ].concat()
+            }
+        }
+
+        match tree {
+            None => vec![],
+            Some(t) => build_symbol_table(t, None)
+        }
+    }
+}
+
 fn main() {
-    let mut args = std::env::args()
-        .skip(1);
+    let mut args = std::env::args().skip(1);
+
+    let mut map: HashMap<char, u64> = std::collections::HashMap::new();
 
     if let Some(filename) = args.next() {
-        let file_buf_reader_res = std::fs::File::open(filename)
-        .map(|file| {
-            std::io::BufReader::new(file)
-        });
+        let file_buf_reader_res =
+            std::fs::File::open(filename).map(|file| std::io::BufReader::new(file));
 
-       let mut buf_reader = match file_buf_reader_res {
+        let mut buf_reader = match file_buf_reader_res {
             Ok(reader) => reader,
             Err(e) => {
-                eprintln!( "Error while reading file: {}", e);
+                eprintln!("Error while reading file: {}", e);
                 print_usage();
                 std::process::exit(1);
             }
         };
 
-        let mut buf:[u8;1024] = [0;1024];
-        let mut map:HashMap<char, u64>= std::collections::HashMap::new();
+        let mut buf: [u8; 1024] = [0; 1024];
         loop {
             let read = match buf_reader.read(&mut buf) {
                 Ok(n) => n,
@@ -31,9 +132,7 @@ fn main() {
             };
             let buf = &buf[..read];
             for byte in buf {
-                let count = map
-                .entry(*byte as char)
-                .or_insert(0);
+                let count = map.entry(*byte as char).or_insert(0);
                 *count += 1;
             }
             if read < 1024 {
@@ -45,9 +144,9 @@ fn main() {
         print_usage();
         std::process::exit(1);
     }
+
+    println!("{:#?}", huffman::encode(&map))
 }
-
-
 
 fn print_usage() {
     eprintln!("Usage: cargo run <filename>");
