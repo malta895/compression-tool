7c62b0a2da3290525418124d573dfb1bfd406e9f
Mattia Fumo <mattia.fumo@mia-platform.eu>
2024-09-30 19:22:52 +0200

    better ratio

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -4,15 +4,16 @@ WINRAT_BIN="target/release/compression-tool"
 set -e
 
 (
-    echo "file gzip_size winrar_size ratio"
+    echo "file file_size gzip_size winrar_size gzip_file_ratio winrar_file_ratio gzip_winrar_ratio"
     for file in fixtures/*; do
         compressed_file="$TMP_DIR/$(basename $file)"
         gzip -c $file > "$compressed_file.gz"
         $WINRAT_BIN -i $file -o "$compressed_file.rar"
 
+        file_size=$(du $file | cut -f1)
         gzip_size=$(du "$compressed_file.gz" | cut -f1)
         winrat_size=$(du "$compressed_file.rar" | cut -f1)
 
-        echo "$file $(du -h "$compressed_file.gz" | cut -f1) $(du -h "$compressed_file.rar" | cut -f1) $(echo "scale=4; $gzip_size / $winrat_size" | bc)"
+        echo "$file $(du -h $file | cut -f1) $(du -h "$compressed_file.gz" | cut -f1) $(du -h "$compressed_file.rar" | cut -f1) $(echo "scale=4; $gzip_size / $file_size" | bc) $(echo "scale=4; $winrat_size / $file_size" | bc) $(echo "scale=4; $gzip_size / $winrat_size" | bc)"
     done
 ) | column -t
